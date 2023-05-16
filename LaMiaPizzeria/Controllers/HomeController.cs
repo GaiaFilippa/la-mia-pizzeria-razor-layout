b92d67b40fe633b9161295a578b5a8c21fb99c4c
GaiaFilippa <gaiafilippa2001@gmail.com>
2023-05-16 17:10:22 +0200

    added Contacts and modified layout

--- LaMiaPizzeria/Controllers/HomeController.cs
+++ LaMiaPizzeria/Controllers/HomeController.cs
@@ -23,6 +23,12 @@ namespace LaMiaPizzeria.Controllers
             return View();
         }
 
+        public IActionResult Contacts()
+        {
+            return View();
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
